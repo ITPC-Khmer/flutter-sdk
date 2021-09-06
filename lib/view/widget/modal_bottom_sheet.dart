64932bcb54719865e8e8207121bfbf1471ba0d22
sonlion <ksmart.lion@gmail.com>
2021-09-06 16:55:41 +0700

    update randomBetween

--- lib/view/widget/modal_bottom_sheet.dart
+++ lib/view/widget/modal_bottom_sheet.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
-Future<void> showModalBottomSheet(BuildContext context,Widget w) async{
+
+Future<void> gShowModalBottomSheet({required BuildContext context,bool expand = false,required widget}) async{
   await showMaterialModalBottomSheet(
+    expand: expand,
     context: context,
-    builder: (context) => SingleChildScrollView(
-      controller: ModalScrollController.of(context),
-      child: w,
-    ),
+    backgroundColor: Colors.transparent,
+    builder: (context) => widget,
   );
 }
