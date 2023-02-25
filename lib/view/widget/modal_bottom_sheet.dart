69e3345711943dc2f5c4d4b4780758179786eb36
globesosuperapp <ksmart.lion@gmail.com>
2023-02-25 21:12:12 +0700

    u

--- lib/view/widget/modal_bottom_sheet.dart
+++ lib/view/widget/modal_bottom_sheet.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+// import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
+Future<void> gShowModalBottomSheet(
+    {required BuildContext context,
+    bool expand = false,
+    required widget}) async {
+  // await showMaterialModalBottomSheet(
+  //   expand: expand,
+  //   context: context,
+  //   backgroundColor: Colors.transparent,
+  //   builder: (context) => widget,
+  // );
 
-Future<void> gShowModalBottomSheet({required BuildContext context,bool expand = false,required widget}) async{
-  await showMaterialModalBottomSheet(
-    expand: expand,
+  await showModalBottomSheet<void>(
     context: context,
-    backgroundColor: Colors.transparent,
-    builder: (context) => widget,
+    builder: (BuildContext context) {
+      return Container(
+        height: 200,
+        color: Colors.grey,
+        child: widget,
+      );
+    },
   );
 }
